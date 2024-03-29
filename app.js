e46bb383d851bc35da3ae71f00f82daab1afd776
priyanshuG15 <priyanshu.garg@rupyy.com>
2024-03-29 21:02:11 +0530

    mongoose added

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+
+const app = express();
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config({ path: 'config/config.env' });
+}
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
+
+
+app.get('/', (req, res) => {
+    res.send('Server is Running! 🚀');
+});
+
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,24 @@
+const app = require('./app');
+const connectDatabase = require('./config/database');
+const PORT = process.env.PORT || 4040;
+
+// UncaughtException Error
+process.on('uncaughtException', (err) => {
+    console.log(`Error: ${err.message}`);
+    process.exit(1);
+});
+
+connectDatabase();
+
+const server = app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+});
+
+// Unhandled Promise Rejection
+process.on('unhandledRejection', (err) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => {
+        process.exit(1);
+    });
+    
+});

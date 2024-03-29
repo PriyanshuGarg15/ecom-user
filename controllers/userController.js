6ac26f1a7d8990e2e97ceef41f00b80f89b5a8d0
priyanshuG15 <priyanshu.garg@rupyy.com>
2024-03-29 22:13:36 +0530

    login user added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,29 @@
 const User = require('../models/user');
 const asyncErrorHandler = require('../middleware/ErrorHandler');
 const pushToken = require('../utils/pushToken');
+const SyncErrorHandler = require('../utils/SyncErrorHandler');
 // Register User
 module.exports = {
     registerUser: asyncErrorHandler(async (req, res, next) => {
         const { name, email, gender, password } = req.body;
         const user = await User.create({ name, email, gender, password });
+        pushToken(user, 201, res);
+    }),
+
+    loginUser : asyncErrorHandler(async (req, res, next) => {
+        const { email, password } = req.body;
+        if(!email || !password) {
+            return next(new SyncErrorHandler("Please Enter Email And Password", 400));
+        }
+        const user = await User.findOne({ email}).select("+password");
+        if(!user) {
+            return next(new SyncErrorHandler("Invalid Email or Password", 401));
+        }
+
+        const isPasswordMatched = await user.comparePassword(password);
+        if(!isPasswordMatched) {
+            return next(new SyncErrorHandler("Invalid Email or Password", 401));
+        }
         pushToken(user, 201, res);
     })
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { registerUser} = require('../controllers/userController');
+const { registerUser, loginUser} = require('../controllers/userController');
 const { isAuthenticatedUser } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.route('/register').post(registerUser);
+router.route('/login').post(loginUser);
 
 module.exports = router;

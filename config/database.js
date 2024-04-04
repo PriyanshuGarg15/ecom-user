3a9d7f7b8ac51c5b5001f4c8cf4540117d26986e
priyanshuG15 <priyanshu.garg@rupyy.com>
2024-04-04 13:52:20 +0530

    consoling env

--- config/database.js
+++ config/database.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const MONGO_URI =process.env.MONGO_URI
 
 const connectDatabase = () => {
+    console.log("mongo: ",MONGO_URI)
     mongoose.connect(MONGO_URI)
         .then(() => {
             console.log("Mongoose Connected");
